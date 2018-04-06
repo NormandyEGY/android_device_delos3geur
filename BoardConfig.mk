04d11643676be4664a5f0dd7f7a09b5099622dcb
yshalsager <ysh-alsager@hotmail.com>
2018-04-06 22:32:19 +0530

    normandy: use msm7x27a as bootloader name

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,7 +18,7 @@
 include device/generic/msm7x27a/BoardConfigCommon.mk
 
 ## Platform
-TARGET_BOOTLOADER_BOARD_NAME := normandy
+TARGET_BOOTLOADER_BOARD_NAME := msm7x27a
 
 ## Kernel
 TARGET_KERNEL_CONFIG := normandy_null_defconfig
